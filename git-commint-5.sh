2a5d706a93c7235c5e9f21514d63ac3c2342a968
Ricotto <Spammumboanddie@gmail.com>
2024-04-25 22:19:23 -0700

    Delete git-commint-5.sh

--- git-commint-5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /var/www/project
-
-git add *
-timestamp(){
-   date +"%d.%m.%Y um %H:%M"
-}
-git commit -am "Auto Server Commit $(timestamp)"
-git push --repo https://[user]:[password]@github.com/[organisation]/[repo].git
